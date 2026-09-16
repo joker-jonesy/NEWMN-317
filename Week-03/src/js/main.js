1c5a29bdde07be3d091ce07f02507f0cc15c2a04
joker-jonesy <spashmcgrath94@yahoo.com>
2026-09-16 11:10:53 -0400

    kermit

--- Week-03/src/js/main.js
+++ Week-03/src/js/main.js
@@ -1,7 +1,27 @@
 import store from './store/index.js'
 import List from "./components/list.js";
+import Status from "./components/status.js"
+
+const formElement = document.querySelector(".js-form");
+const inputElement = document.querySelector(".new-item-field");
+const errorElement = document.querySelector(".error");
+
+formElement.addEventListener("submit", event =>{
+    event.preventDefault();
+
+    let  value = inputElement.value;
+
+    if(value.length && value.length>=8){
+        store.dispatch("addItem", value);
+        errorElement.innerHTML = "";
+    } else{
+        errorElement.innerHTML = "Incorrect Amount of characters to submit";
+    }
+})
 
 const listInstance = new List();
+const statusInstance = new Status();
 listInstance.render()
+statusInstance.render();
 
 
